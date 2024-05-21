c457252621deeb18a4b9b603683c2856d9113b92
vale97408 <vale97408@gmail.com>
2024-05-21 11:43:45 -0300

    1erCommit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// PRUEBA DE USO DE C# 
+Console.WriteLine("Hello, World!");
+
+
+
+// ==================EJERCICIO 1=========================
+
+   
